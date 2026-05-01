37eecea9930ad91e3ed79a585bbd070ef67ccca3
Pedro Pires <2102824@estudante.uab.pt>
2026-05-02 00:12:05 +0100

    fix do JWT property loading fallback"

--- gestao-clubes-api/src/main/java/com/gestaoclubes/api/GestaoClubesApiApplication.java
+++ gestao-clubes-api/src/main/java/com/gestaoclubes/api/GestaoClubesApiApplication.java
@@ -2,8 +2,15 @@ package com.gestaoclubes.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true),
+        @PropertySource(value = "file:src/main/resources/application.properties", ignoreResourceNotFound = true),
+        @PropertySource(value = "file:gestao-clubes-api/src/main/resources/application.properties", ignoreResourceNotFound = true)
+})
 public class GestaoClubesApiApplication {
 
     public static void main(String[] args) {
