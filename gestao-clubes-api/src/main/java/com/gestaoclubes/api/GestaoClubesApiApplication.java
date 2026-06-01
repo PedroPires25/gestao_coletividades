d04f98696c4a1627acbf2b9d10a182c23502e336
Pedro Pires <2102824@estudante.uab.pt>
2026-06-01 10:26:19 +0100

    remover configuração redundante de PropertySources

--- gestao-clubes-api/src/main/java/com/gestaoclubes/api/GestaoClubesApiApplication.java
+++ gestao-clubes-api/src/main/java/com/gestaoclubes/api/GestaoClubesApiApplication.java
@@ -2,17 +2,10 @@ package com.gestaoclubes.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@PropertySources({
-        @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true),
-        @PropertySource(value = "file:src/main/resources/application.properties", ignoreResourceNotFound = true),
-        @PropertySource(value = "file:gestao-clubes-api/src/main/resources/application.properties", ignoreResourceNotFound = true)
-})
 public class GestaoClubesApiApplication {
 
     public static void main(String[] args) {
